606247b8e4c8f395edc9d80521025447be26a216
Vinicius <74621269+vcsil@users.noreply.github.com>
2021-02-16 11:28:36 -0300

    Update 07 - Validando imagens.py

--- 07 - Validando imagens.py
+++ 07 - Validando imagens.py
@@ -5,41 +5,9 @@ Created on Fri Jan 29 10:21:33 2021
 @author: vinic
 """
 
-'''import os
-import cv2
-bad_list=[]
-dir=r'C:\\Users\\vinic\\Downloads\\Spyder\\Independente\\02 - Pothole'
-subdir_list=os.listdir(dir) # create a list of the sub directories in the directory ie train or test
-for d in subdir_list:  # iterate through the sub directories train and test
-    dpath=os.path.join (dir, d) # create path to sub directory
-    if d in ['archive']:
-        class_list=os.listdir(dpath) # list of classes ie dog or cat
-       # print (class_list)
-        for klass in class_list: # iterate through the two classes
-            class_path=os.path.join(dpath, klass) # path to class directory
-            #print(class_path)
-            file_list=os.listdir(class_path) # create list of files in class directory
-            for f in file_list: # iterate through the files
-                fpath=os.path.join (class_path,f)
-                index=f.rfind('.') # find index of period infilename
-                ext=f[index+1:] # get the files extension
-                if ext  not in ['jpg', 'png', 'bmp', 'gif']:
-                    print(f'file {fpath}  has an invalid extension {ext}')
-                    bad_list.append(fpath)                    
-                else:
-                    try:
-                        img=cv2.imread(fpath)
-                        size=img.shape
-                    except:
-                        print(f'file {fpath} is not a valid image file ')
-                        bad_list.append(fpath)
-                       
-print (bad_list)'''
-
-
 import os
 from PIL import Image
-folder_path = 'C:/Users/vinic/Downloads/Dataset/01/finish'
+folder_path = ''
 extensions = []
 erros = []
 for fldr in os.listdir(folder_path):
